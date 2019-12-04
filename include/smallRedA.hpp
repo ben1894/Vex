a134cde4bd18de8e9a788655e4f2c54d9c9c846e
ben1894 <50091002+ben1894@users.noreply.github.com>
2019-12-03 21:27:04 -0600

    Dedicated Auton File

---
+++ include/smallRedA.hpp
@@ -0,0 +1,51 @@
+#pragma once
+#include "main.h"
+#include "forwardDeclairations.hpp"
+
+void smallRed()
+{
+    gyro.reset();
+    tilter.tare_position();
+all( //ignore error, it compiles
+    DRIVE,FORWARDS,1500,0,MMREGPID,60,127,
+    DRIVE,FORWARDS,1000,0,NOPID,60,
+    DRIVE,TURN,3000,NOSTRAIGHT,TURNPID,
+    DRIVE,BACKWARDS,3000,3000,
+    DRIVE,TURN,0,NOSTRAIGHT,TURNPID,
+
+    );
+
+    /*
+        DRIVE,UPLEFTSWEEP,10000,300,NOSTRAIGHT,MMREGPID,40,127,
+    DRIVE,DOWNRIGHTSWEEP,10000,300,NOSTRAIGHT,MMREGPID,40,127,
+    DRIVE,DOWNLEFTSWEEP,10000,300,NOSTRAIGHT,MMREGPID,40,127,
+    DRIVE,UPRIGHTSWEEP,10000,300,NOSTRAIGHT,MMREGPID,40,127
+    gyro.reset();
+    tilter.tare_position();
+all(DRIVE,FORWARDS,1300,NOSTRAIGHT,NOPID,60,
+    DRIVE,FORWARDS,450,NOSTRAIGHT,NOPID,60,TIMET,600,
+    DRIVE,FORWARDS,450,NOSTRAIGHT,NOPID,60,TIMET,600,
+    DRIVE,FORWARDS,450,NOSTRAIGHT,NOPID,60,TIMET,600,
+    DRIVE,FORWARDS,580,NOSTRAIGHT,NOPID,60,TIMET,600,
+    DRIVE,BACKWARDS,1860,NOSTRAIGHT,TIMET,100,
+    DRIVE,TURN,1210,NOSTRAIGHT,TURNPID,
+    INTAKE,OUT,127,
+    INTAKE,IN,127,DRIVET,1,230);
+    driveMotorsSpeed(110,leftDrive);
+    driveMotorsSpeed(110,rightDrive);
+    pros::delay(520);
+    driveMotorsSpeed(0,leftDrive);   /////////////////////red small
+    driveMotorsSpeed(0,rightDrive);
+    pros::delay(500);
+    motorGroupMove(-100,intakeM);
+    pros::delay(550);
+    motorGroupMove(0,intakeM);
+    all(TILTER,POSITION,7100,100);
+    pros::delay(2000);
+    driveMotorsSpeed(-50,leftDrive);
+    driveMotorsSpeed(-50,rightDrive);
+    pros::delay(1500);
+    driveMotorsSpeed(0,leftDrive);
+    driveMotorsSpeed(0,rightDrive);
+    */
+}
--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -571,11 +571,11 @@ class Drive : public System
 
         if(difference > 4)
         {
-            rightCorrect *= ((float).97 - ((float)abs(difference)/(float)100));
+            rightCorrect *= ((float).98 - ((float)abs(difference)/(float)100));
         }
         else if(difference < -4)
         {
-            leftCorrect  *= ((float).97 - ((float)abs(difference)/(float)100));
+            leftCorrect  *= ((float).98 - ((float)abs(difference)/(float)100));
         }
     }
 
@@ -1123,47 +1123,8 @@ void all(Ts... input)
 }
 
 //DRIVE, distance, correctTo(value,CURRENTVAL,NOSTRAIGHT,WHEELCORRECTION), {speedControl - REGPID(NOPID, MMPID)} //7280 max for tilter
-void smallRed()
-{
-    gyro.reset();
-    tilter.tare_position();
-all(
-    DRIVE,UPLEFTSWEEP,10000,300,NOSTRAIGHT,MMREGPID,40,127,
-    DRIVE,DOWNRIGHTSWEEP,10000,300,NOSTRAIGHT,MMREGPID,40,127,
-    DRIVE,DOWNLEFTSWEEP,10000,300,NOSTRAIGHT,MMREGPID,40,127,
-    DRIVE,UPRIGHTSWEEP,10000,300,NOSTRAIGHT,MMREGPID,40,127
-    );
 
-    /*
-    gyro.reset();
-    tilter.tare_position();
-all(DRIVE,FORWARDS,1300,NOSTRAIGHT,NOPID,60,
-    DRIVE,FORWARDS,450,NOSTRAIGHT,NOPID,60,TIMET,600,
-    DRIVE,FORWARDS,450,NOSTRAIGHT,NOPID,60,TIMET,600,
-    DRIVE,FORWARDS,450,NOSTRAIGHT,NOPID,60,TIMET,600,
-    DRIVE,FORWARDS,580,NOSTRAIGHT,NOPID,60,TIMET,600,
-    DRIVE,BACKWARDS,1860,NOSTRAIGHT,TIMET,100,
-    DRIVE,TURN,1210,NOSTRAIGHT,TURNPID,
-    INTAKE,OUT,127,
-    INTAKE,IN,127,DRIVET,1,230);
-    driveMotorsSpeed(110,leftDrive);
-    driveMotorsSpeed(110,rightDrive);
-    pros::delay(520);
-    driveMotorsSpeed(0,leftDrive);   /////////////////////red small
-    driveMotorsSpeed(0,rightDrive);
-    pros::delay(500);
-    motorGroupMove(-100,intakeM);
-    pros::delay(550);
-    motorGroupMove(0,intakeM);
-    all(TILTER,POSITION,7100,100);
-    pros::delay(2000);
-    driveMotorsSpeed(-50,leftDrive);
-    driveMotorsSpeed(-50,rightDrive);
-    pros::delay(1500);
-    driveMotorsSpeed(0,leftDrive);
-    driveMotorsSpeed(0,rightDrive);
-    */
-}
+#include "smallRedA.hpp"
 
 void smallBlue()
 {
@@ -1247,12 +1208,4 @@ void autonomous()
         case(THICCBLUE):
             thiccBlue();*/
         //thiccRed();
-    }
-/*
-
-    */
-
-
-/*
-
-*/
+}
